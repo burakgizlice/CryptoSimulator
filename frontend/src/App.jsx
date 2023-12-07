dbc3c61ec893cf43692ae64bd6a3bfd15be324fa
Burak GIZLICE <87895075+Downshifter114@users.noreply.github.com>
2023-12-07 11:34:51 +0300

    Tailwind Installed, React Router Set Up
    Installed TailwindCSS and tested it, installed react router and set up the routing logic.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/common/Navbar/Navbar";
+import Footer from "./components/common/Footer/Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={<h1>Main Page</h1>}
+				/>
+				<Route
+					path="/about"
+					element={<h1>About Page</h1>}
+				/>
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	</React.StrictMode>
+);
